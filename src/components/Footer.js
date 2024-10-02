85f326ec98c41ce0874a235eb10b6088944572ba
pathakprashant7 <prashant409997@gmail.com>
2024-10-02 10:03:50 +0530

    Footer Done for desktop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className="footer">
-      <a href="https://linkedin.com/in/prashantpathak7" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-      <a href="https://github.com/prashantpathak7" target="_blank" rel="noopener noreferrer">GitHub</a>
-    </footer>
-  );
+
+    <div className='footer-container'>
+        <div className='section1-left'>
+            <div className='get-in-touch'>
+              GET IN TOUCH
+            </div>
+            <div className='emailid'>
+              prashant409997@gmail.com
+            </div>
+            <div className='address'>
+              Pune, India
+            </div>
+            <div className='DevelopedBy'>
+              Developed with ♥ and ReactJS + Firebase
+            </div>
+        </div>
+
+        <div className='section2-Right'>
+            <div className='get-in-touch'>
+                LET'S CONNECT
+            </div>
+            <div className='linkedIn'>
+            <a href="https://www.linkedin.com/in/prashantpathak7/" target="_blank" class="link">LinkedIn</a>
+            </div>
+
+
+            <div className='linkedIn'>
+            <a href="https://www.fiverr.com/pathakprashant" target="_blank" class="link">Fiverr Reviews</a>
+            </div>
+
+            <div className='linkedIn'>
+            <a href="https://github.com/prashantpathak7" target="_blank" class="link">GitHub</a>
+            </div>
+
+        </div>
+
+    </div>
+      
+    );
 }
 
 export default Footer;
